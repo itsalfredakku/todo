f00ccf94bca3f8e5c7de113ea6f3c4faf99a7801
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-10-26 13:38:11 +0000

    Changes to be committed: 	modified:   backend/Cargo.lock 	modified:   backend/Cargo.toml 	new file:   backend/src/api.rs 	new file:   backend/src/db.rs 	modified:   backend/src/main.rs 	modified:   database/000004.log
    Changes not staged for commit:
    	modified:   .devcontainer/devcontainer.json
    	modified:   .devcontainer/postStartCommand.sh
    	modified:   README.md
    	deleted:    backend/.env
    	deleted:    backend/.gitignore
    	deleted:    backend/Cargo.lock
    	deleted:    backend/Cargo.toml
    	deleted:    backend/migrations/20230725104030_todo.sql
    	deleted:    backend/src/api.rs
    	deleted:    backend/src/db.rs
    	deleted:    backend/src/main.rs
    	deleted:    database/000004.log
    	deleted:    database/CURRENT
    	deleted:    database/IDENTITY
    	deleted:    database/LOCK
    	deleted:    database/LOG
    	deleted:    database/MANIFEST-000005
    	deleted:    database/OPTIONS-000007
    	deleted:    frontend/.eslintignore
    	deleted:    frontend/.eslintrc.cjs
    	deleted:    frontend/.gitignore
    	deleted:    frontend/.npmrc
    	deleted:    frontend/.prettierignore
    	deleted:    frontend/.prettierrc
    	deleted:    frontend/.vscode/settings.json
    	deleted:    frontend/package-lock.json
    	deleted:    frontend/package.json
    	deleted:    frontend/postcss.config.cjs
    	deleted:    frontend/src/app.d.ts
    	deleted:    frontend/src/app.html
    	deleted:    frontend/src/routes/+layout.svelte
    	deleted:    frontend/src/routes/+page.svelte
    	deleted:    frontend/src/routes/+page.ts
    	deleted:    frontend/svelte.config.js
    	deleted:    frontend/tailwind.config.cjs
    	deleted:    frontend/tsconfig.json
    	deleted:    frontend/vite.config.ts
    
    Untracked files:
    	api/
    	data/
    	web/

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,98 +1,27 @@
-use axum::{
-    extract::{Path, State},
-    routing::{get, post},
-    Form, Json, Router,
-};
-use axum::response::Redirect;
-use axum_error::Result;
-use serde::{Deserialize, Serialize};
+mod db;
+mod api;
+
+use axum::{Router, routing::get, routing::post};
 use sqlx::sqlite::SqlitePool;
 use std::net::SocketAddr;
 use tower_http::cors::CorsLayer;
+use dotenv::dotenv;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    // Create database with connection and share it using with_state below
-    let _ = dotenv::dotenv();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     let url = std::env::var("DATABASE_URL")?;
     let pool = SqlitePool::connect(&url).await?;
 
-    // Create router for server
     let app = Router::new()
-        .route("/", get(list))
-        .route("/create", post(create))
-        .route("/read/:id", get(read))
-        .route("/update", get(update))
-        .route("/delete/:id", post(delete))
-        .with_state(pool)
+        .nest("/", api::router())
+        .with_state(pool.clone())
         .layer(CorsLayer::very_permissive());
 
-    // Start server!
     let address = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("Starting server on http://{address}");
     Ok(axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await?)
-}
-
-#[derive(Deserialize)]
-struct NewTodo {
-    description: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Todo {
-    id: i64,
-    description: String,
-    done: bool,
-}
-
-async fn list(State(pool): State<SqlitePool>) -> Result<Json<Vec<Todo>>> {
-    // List all notes
-    let todos = sqlx::query_as!(Todo, "SELECT id, description, done FROM todos ORDER BY id")
-        .fetch_all(&pool)
-        .await?;
-    Ok(Json(todos))
-}
-
-async fn create(State(pool): State<SqlitePool>, Form(todo): Form<NewTodo>) -> Result<Redirect> {
-    // Create new note
-    sqlx::query!(
-        "INSERT INTO todos (description) VALUES (?)",
-        todo.description,
-    )
-    .execute(&pool)
-    .await?;
-    Ok(Redirect::to("https://rust-svelte-web.devstroop.com"))
-}
-
-async fn read(State(pool): State<SqlitePool>, Path(id): Path<i64>) -> Result<Json<Todo>> {
-    // Read todo
-    let todo = sqlx::query_as!(
-        Todo,
-        "SELECT id, description, done FROM todos WHERE id = ?",
-        id
-    )
-    .fetch_one(&pool)
-    .await?;
-    Ok(Json(todo))
-}
-
-async fn update(State(pool): State<SqlitePool>, Form(todo): Form<Todo>) -> Result<Redirect> {
-    // Update todo
-    sqlx::query!(
-        "UPDATE todos SET description = ?, DONE = ? WHERE id = ?",
-        todo.description,
-        todo.done,
-        todo.id
-    ).execute(&pool).await?;
-    Ok(Redirect::to("https://rust-svelte-web.devstroop.com"))
-}
-
-async fn delete(State(pool): State<SqlitePool>, Path(id): Path<i64>) -> Result<Redirect> {
-    // Update todo
-    sqlx::query!("DELETE FROM todos WHERE id = ?", id)
-        .execute(&pool)
-        .await?;
-    Ok(Redirect::to("https://rust-svelte-web.devstroop.com"))
 }
